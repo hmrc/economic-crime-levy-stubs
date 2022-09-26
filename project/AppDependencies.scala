37d1e5467fc660a030243f7589db2fa3de2cfead
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2022-09-26 13:42:28 +0100

    Update mockito and scalacheck

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,8 +8,8 @@ object AppDependencies {
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-28" % "7.4.0",
-    "org.mockito"         %% "mockito-scala"          % "1.16.42",
-    "org.scalacheck"      %% "scalacheck"             % "1.15.4"
+    "org.mockito"         %% "mockito-scala"          % "1.17.12",
+    "org.scalacheck"      %% "scalacheck"             % "1.17.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
