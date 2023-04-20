652547884d25b3cdad6ae1ce2320f385661d62e6
TonyCOgilvie <111900771+TonyCOgilvie@users.noreply.github.com>
2023-04-20 13:28:39 +0100

    Addressed PR review comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,18 @@ import sbt._
 
 object AppDependencies {
 
+  private val hmrcBootstrapVersion = "7.15.0"
+  private val hmrcMongoVersion     = "1.1.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "7.15.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % hmrcBootstrapVersion,
     "uk.gov.hmrc"       %% "tax-year"                  % "3.0.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "1.1.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVersion
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-28"   % "7.15.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28"  % "1.1.0",
+    "uk.gov.hmrc"       %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion,
     "org.mockito"       %% "mockito-scala"            % "1.17.12",
     "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"
   ).map(_ % "test, it")
