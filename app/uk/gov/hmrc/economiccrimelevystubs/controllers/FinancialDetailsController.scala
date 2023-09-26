888a5f988f35b4a6f3dbcb4886bc65d807ae71d9
nemanjatomanovic <nemanja.tomanovic@gmail.com>
2023-09-26 11:25:35 +0100

    ECL-483-ECL-527: Payments - Interests On Overdue Payment

--- app/uk/gov/hmrc/economiccrimelevystubs/controllers/FinancialDetailsController.scala
+++ app/uk/gov/hmrc/economiccrimelevystubs/controllers/FinancialDetailsController.scala
@@ -37,6 +37,7 @@ class FinancialDetailsController @Inject() (
       case "005" => Ok(readFileService.readFile("FinancialDataPaidObligationResponse"))
       case "006" => Ok(readFileService.readFile("FinancialDataPartiallyPaidResponse"))
       case "007" => Ok(readFileService.readFile("FinancialDataPaidPartiallyPaidOverdueResponse"))
+      case "008" => Ok(readFileService.readFile("FinancialDataPaidObligationPaidPartialPaidInterestResponse"))
       case "400" =>
         BadRequest(
           Json.obj(
