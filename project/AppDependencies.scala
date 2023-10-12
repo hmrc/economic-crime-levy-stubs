47bdc3050e5eb3aae44f9c1a0e225bee61d1e464
Patrick Lucas <patrick.lucas@opencastsoftware.com>
2023-10-12 13:21:06 +0100

    ECL-614: Updated dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,19 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "7.19.0"
+  private val hmrcBootstrapVersion = "7.22.0"
   private val hmrcMongoVersion     = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"       %% "tax-year"                  % "3.2.0",
+    "uk.gov.hmrc"       %% "tax-year"                  % "3.3.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVersion
   )
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion,
-    "org.mockito"       %% "mockito-scala"            % "1.17.12",
+    "org.mockito"       %% "mockito-scala"            % "1.17.25",
     "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"
   ).map(_ % "test, it")
 
