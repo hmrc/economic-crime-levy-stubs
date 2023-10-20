6bd98bca135b920538429a3cf0cde1431b6d1be2
Nemanja Tomanovic <nemanja.tomanovic@gmail.com>
2023-10-20 11:32:13 +0100

    ECL-638: Interest Charge not shown properly when clearing reason is missing (#39)

--- test/uk/gov/hmrc/economiccrimelevystubs/controllers/FinancialDetailsControllerSpec.scala
+++ test/uk/gov/hmrc/economiccrimelevystubs/controllers/FinancialDetailsControllerSpec.scala
@@ -102,6 +102,15 @@ class FinancialDetailsControllerSpec extends SpecBase {
       status(result) shouldBe OK
     }
 
+    "return 200 OK when idNumber ends in '017'" in {
+      when(mockReadFileService.readFile(any()))
+        .thenReturn(Json.parse("{\"getFinancialData\": {}}"))
+
+      val result: Future[Result] =
+        controller.getFinancialDetails(idType, "XMECL0000000017", regimeType)(fakeRequest)
+
+      status(result) shouldBe OK
+    }
     "return 500 INTERNAL_SERVER_ERROR with a SERVER_ERROR code when the idNumber ends in '500'" in {
       val result: Future[Result] =
         controller.getFinancialDetails(idType, "XMECL0000000500", regimeType)(fakeRequest)
