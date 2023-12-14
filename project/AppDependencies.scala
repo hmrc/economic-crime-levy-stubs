c80d6d7d234574de84f43a463120634171cf0b6d
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2023-12-14 14:14:46 +0000

    Play-30 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "7.22.0"
-  private val hmrcMongoVersion     = "1.3.0"
+  private val hmrcBootstrapVersion = "8.2.0"
+  private val hmrcMongoVersion     = "1.6.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % hmrcBootstrapVersion,
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % hmrcBootstrapVersion,
     "uk.gov.hmrc"       %% "tax-year"                  % "3.3.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVersion
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"   % hmrcBootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
     "org.mockito"       %% "mockito-scala"            % "1.17.25",
     "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"
   ).map(_ % "test, it")
