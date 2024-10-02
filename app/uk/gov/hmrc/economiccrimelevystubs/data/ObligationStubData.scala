c2bd5e757a7c7eb9da083f3d9eb0c4bbe9189e0b
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2024-10-02 13:34:16 +0100

    Amending the obligation stub data for the 005 scenario to cater for the movement of the due date. (#58)

--- app/uk/gov/hmrc/economiccrimelevystubs/data/ObligationStubData.scala
+++ app/uk/gov/hmrc/economiccrimelevystubs/data/ObligationStubData.scala
@@ -139,7 +139,7 @@ object ObligationStubData {
             inboundCorrespondenceFromDate = periodFrom(currentYear - 1),
             inboundCorrespondenceToDate = periodTo(currentYear - 1),
             inboundCorrespondenceDateReceived = None,
-            inboundCorrespondenceDueDate = dueDate(periodTo(currentYear - 1).getYear),
+            inboundCorrespondenceDueDate = dueDate(periodTo(currentYear).getYear),
             periodKey = periodKey(periodFrom(currentYear))
           )
         )
