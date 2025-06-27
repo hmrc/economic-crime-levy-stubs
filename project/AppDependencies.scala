0e8a37c3ddecf486cb112ae4903c3def83698ef1
Vic Watson <171040641+VJWatson@users.noreply.github.com>
2025-06-27 14:40:49 +0100

    DDCYLS-6951:app deps (#65)
    * app deps
    
    * platOps bot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,20 +1,20 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "9.11.0"
+  private val hmrcBootstrapVersion = "9.13.0"
   private val hmrcMongoVersion     = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"       %% "tax-year"                  % "5.0.0",
+    "uk.gov.hmrc"       %% "tax-year"                  % "6.0.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion
   )
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"   % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
-    "org.mockito"       %% "mockito-scala"            % "1.17.30",
+    "org.mockito"       %% "mockito-scala"            % "2.0.0",
     "org.scalatestplus" %% "scalacheck-1-17"          % "3.2.18.0"
   ).map(_ % "test, it")
 
